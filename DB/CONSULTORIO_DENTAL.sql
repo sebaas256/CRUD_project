7778c1f2efb93cd26ad2dc7b5ed29dbc91823c62
sebaas256 <csebas220@gmail.com>
2025-05-24 18:47:58 -0600

    arreglo del script de la BD

--- DB/CONSULTORIO_DENTAL.sql
+++ DB/CONSULTORIO_DENTAL.sql
@@ -53,26 +53,11 @@ CREATE TABLE DOC_ESPC
 	Nombre_esp varchar(40) not null,
 )
 
-CREATE TABLE CONSULT_TRATAM
-(
-	Id_Cnslt_Trat int not null,
-	Id_Consulta int not null,
-	Id_Det_Trat int not null,
-)
-
 CREATE TABLE DETA_TRATAM
 (
 	Id_Det_Trat int not null,
 	deta_trat varchar(200) not null,
-	Id_Pago int not null,
-)
-
-CREATE TABLE DETALLE_PAGO
-(
-	Id_Pago int not null,
-	pago_consulta money not null,
-	pago_tratamiento money not null,
-	pago_total money not null
+	Id_Consulta int not null
 )
 
 -- CREACION DE LAS LLAVES PRIMARIAS
@@ -96,17 +81,10 @@ ALTER TABLE DOC_ESPC
 ADD CONSTRAINT pk_doc_espc
 PRIMARY KEY (Id_Espc)
 
-ALTER TABLE DETALLE_PAGO
-ADD CONSTRAINT pk_Det_Pago
-PRIMARY KEY (Id_Pago)
-
 ALTER TABLE DETA_TRATAM
 ADD CONSTRAINT pk_Det_Trat
 PRIMARY KEY (Id_Det_Trat)
 
-ALTER TABLE CONSULT_TRATAM
-ADD CONSTRAINT pk_Cnslt_Trat
-PRIMARY KEY (Id_Cnslt_Trat)
 
 -- CREACION DE LLAVES FORANEAS
 
@@ -114,34 +92,28 @@ ALTER TABLE DOCTORES
 ADD CONSTRAINT fk_doc_espc
 FOREIGN KEY (Id_Espc)	
 REFERENCES DOC_ESPC(Id_Espc)
+on update cascade on delete no action
 
 ALTER TABLE CONSULTAS 
 ADD CONSTRAINT fk_cons_doc
 FOREIGN KEY (Id_Doctor)
 REFERENCES DOCTORES(Id_Doctor)
+on update cascade on delete no action
 
 ALTER TABLE CONSULTAS 
 ADD CONSTRAINT fk_cons_cita
 FOREIGN KEY (Id_Cita)
 REFERENCES CITAS(Id_Cita)
+on update cascade on delete no action
 
 ALTER TABLE CITAS 
 ADD CONSTRAINT fk_cita_pac
 FOREIGN KEY (Dui_Paciente)
 REFERENCES PACIENTES(Dui_Paciente)
-
-ALTER TABLE CONSULT_TRATAM
-ADD CONSTRAINT fk_constram_consulta
-FOREIGN KEY (Id_Consulta)
-REFERENCES CONSULTAS(Id_Consulta)
-
-ALTER TABLE CONSULT_TRATAM
-ADD CONSTRAINT fk_cosntram_detatram
-FOREIGN KEY (Id_Det_Trat)
-REFERENCES DETA_TRATAM (Id_Det_Trat)
+on update cascade on delete no action
 
 ALTER TABLE DETA_TRATAM
-ADD CONSTRAINT fk_detatram_detpago
-FOREIGN KEY (Id_Pago)
-REFERENCES DETALLE_PAGO (Id_Pago)
-
+ADD CONSTRAINT fk_detatram_consult
+FOREIGN KEY (Id_Consulta)
+REFERENCES CONSULTAS (Id_Consulta)
+on update cascade on delete no action
